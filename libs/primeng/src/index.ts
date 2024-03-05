51e3868ccc2d6184eb2938f3f7aaf80b12560cca
Rusty Green <me@rusty.green>
2024-03-05 09:56:14 -0500

    chore: bumped version

--- libs/primeng/src/index.ts
+++ libs/primeng/src/index.ts
@@ -2,5 +2,6 @@ export * from './lib/provide-supabase';
 export * from './lib/login/login.component';
 export * from './lib/set-password/set-password.component';
 export * from './lib/reset-password/reset-password.component';
+export * from './lib/loading-overlay/loading-overlay.component';
 
 export * from './lib/register/register.component';
